a0a02ee134463c25e0f2a7fc3c066b71f4dc80d9
l37sg0 <petar_1v@abv.bg>
2023-03-10 09:45:46 +0200

    [#3]  - image input rework.  - band section template.

--- rockstar/app/Website/resources/views/admin/band.blade.php
+++ rockstar/app/Website/resources/views/admin/band.blade.php
@@ -1,7 +1,43 @@
 <x-rockstar::layout>
     <x-slot:header>Band</x-slot:header>
     <h1>Band edit Section</h1>
-    <x-image-input/>
-    <x-image-input/>
-    <x-image-input/>
+
+    <div class="flex flex-row ml-3">
+        <div class="basis-1/3">
+            <x-image-input/>
+            <x-input-label for="name" :value="__('Member Name')"/>
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
+                          autofocus autocomplete="name"/>
+            <x-input-error :messages="$errors->get('name')" class="mt-2"/>
+            <x-input-label for="shortDesc" :value="__('Short Description')"/>
+            <x-text-input id="shortDesc" class="block mt-1 w-full" type="text" name="shortDesc"
+                          :value="old('shortDesc')" required
+                          autofocus autocomplete="shortDesc"/>
+            <x-input-error :messages="$errors->get('shortDesc')" class="mt-2"/>
+        </div>
+        <div class="basis-1/3">
+            <x-image-input/>
+            <x-input-label for="name" :value="__('Member Name')"/>
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
+                          autofocus autocomplete="name"/>
+            <x-input-error :messages="$errors->get('name')" class="mt-2"/>
+            <x-input-label for="shortDesc" :value="__('Short Description')"/>
+            <x-text-input id="shortDesc" class="block mt-1 w-full" type="text" name="shortDesc"
+                          :value="old('shortDesc')" required
+                          autofocus autocomplete="shortDesc"/>
+            <x-input-error :messages="$errors->get('shortDesc')" class="mt-2"/>
+        </div>
+        <div class="basis-1/3">
+            <x-image-input/>
+            <x-input-label for="name" :value="__('Member Name')"/>
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
+                          autofocus autocomplete="name"/>
+            <x-input-error :messages="$errors->get('name')" class="mt-2"/>
+            <x-input-label for="shortDesc" :value="__('Short Description')"/>
+            <x-text-input id="shortDesc" class="block mt-1 w-full" type="text" name="shortDesc"
+                          :value="old('shortDesc')" required
+                          autofocus autocomplete="shortDesc"/>
+            <x-input-error :messages="$errors->get('shortDesc')" class="mt-2"/>
+        </div>
+    </div>
 </x-rockstar::layout>
